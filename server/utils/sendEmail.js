9c2a2406646f1e69ced8d8c9dfd22c1634afc999
Nitinrathor051 <nitinrathor0505feaid@gmail.com>
2026-07-20 09:27:29 +0530

    Configure Brevo SMTP

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -1,25 +1,19 @@
 import nodemailer from "nodemailer";
 
 const transporter = nodemailer.createTransport({
-  host: "smtp.gmail.com",
-  port: 587,
-  secure: false,
-  requireTLS: true,
-
+  host: process.env.EMAIL_HOST,
+  port: Number(process.env.EMAIL_PORT),
+  secure: false, // true only for port 465
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS,
   },
 });
 
-export const sendEmail = async (
-  email,
-  subject,
-  message
-) => {
+export const sendEmail = async (email, subject, message) => {
   try {
     await transporter.sendMail({
-      from: `"BaseraMitra" <${process.env.EMAIL_USER}>`,
+      from: process.env.EMAIL_FROM,
       to: email,
       subject,
       text: message,
