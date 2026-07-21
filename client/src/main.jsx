941125b7194e0d92e7a37c9998995842d6465a27
Nitinrathor051 <nitinrathor0505feaid@gmail.com>
2026-07-21 17:53:23 +0530

    Add ToastContainer for notifications

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 import "./index.css";
 
@@ -12,6 +14,17 @@ createRoot(document.getElementById("root")).render(
     <BrowserRouter>
       <AuthProvider>
         <AppRoutes />
+
+        <ToastContainer
+          position="top-right"
+          autoClose={3000}
+          hideProgressBar={false}
+          newestOnTop
+          closeOnClick
+          pauseOnHover
+          draggable
+          theme="colored"
+        />
       </AuthProvider>
     </BrowserRouter>
   </StrictMode>
